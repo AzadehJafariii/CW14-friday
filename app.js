b6eff1bc65fd9f9259bc10dc0b0a43820f26c7da
marziehmokhtari <marziehmokhtari67@gmail.com>
2022-07-01 15:25:37 +0430

    Edit api get

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const API_URL = "https://62ac4e7dbd0e5d29af1fac5b.mockapi.io/api/todosList";
 
 $(document).ready(function () {
+  const table =$(".tableBody")
   $.ajax({
     url: API_URL,
-  }).done(function (todos) {
-    console.log(todos);
+  }).done(function (data) {
+    const{todosList}=data
+    $.each(todosList,function(_,todo){
+      table.append(
+        `<tr data-id=${todo.id}><td><i class="bi bi-check" style=${todo.status ? "display:none" : "display:block"} ></i></td><td>${todo.todos}</td><td><i class="bi bi-x"></i></td></tr>`
+      )
+    })
+    
+
   });
 });
